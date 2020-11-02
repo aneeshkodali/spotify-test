72f902e8d7ca12ffcbd8415ed66a524327abaed6
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-11-02 11:36:15 -0500

    Connect Client and Server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+
+    const [test, setTest] = useState(null);
+    const search = async () => {
+        const response = await axios.get('http://localhost:5000');
+        const { data } = response;
+        console.log(response)
+    }
+    useEffect(() => {
+        search()
+    }, [])
+
     return (
         <div>
             APP
--- node_server/index.js
+++ node_server/index.js
@@ -1,12 +1,36 @@
-const app = require('express')();
+// IMPORTS
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+// APP CONFIG
+const app = express()
+app.use(cors());
 
 
+// SPOTIFY API SETUP
+const SpotifyWebApi = require('spotify-web-api-node');
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_SECRET_ID
+});
+spotifyApi.setAccessToken(process.env.SPOTIFY_TOKEN)
 
 app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+    //res.send("Hello World!")
+    spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+        function(data) {
+        //  console.log('Artist albums', data.body);
+        res.send(data.body)
+        },
+        function(err) {
+          console.error(err);
+        }
+      );
+});
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server now running on port ${port}`)
 })
